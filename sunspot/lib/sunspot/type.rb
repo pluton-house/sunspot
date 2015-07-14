89c9602eb9a9b15450cf1d3b6aac900458d92677
Clive Miller <clive.miller@thomsonreuters.com>
2015-07-14 11:08:15 +0100

    Added autocomplete and autosuggest

--- sunspot/lib/sunspot/type.rb
+++ sunspot/lib/sunspot/type.rb
@@ -409,5 +409,35 @@ module Sunspot
       end
     end
     register ClassType, Class
+    
+    # Auto Completion
+    class AutocompleteType < AbstractType
+      def indexed_name(name) #:nodoc:
+        "#{name}_ac"
+      end
+      
+      def to_indexed(value) #:nodoc:
+        value.to_s if value
+      end
+      
+      def cast(string) #:nodoc:
+        string
+      end
+    end
+    
+    # Auto Suggestion
+    class AutosuggestType < AbstractType
+      def indexed_name(name) #:nodoc:
+        "#{name}_as"
+      end
+      
+      def to_indexed(value) #:nodoc:
+        value.to_s if value
+      end
+      
+      def cast(string) #:nodoc:
+        string
+      end
+    end
   end
 end
